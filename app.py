0e767aeca723da9ff17f29f5b4058669450cda35
andhikayp <andhikay24@gmail.com>
2018-09-23 21:24:14 +0700

    Oke

--- app.py
+++ app.py
@@ -69,12 +69,11 @@ app = Flask(__name__)
 
 
 # Channel Access Token
-
 line_bot_api = LineBotApi('p5oHtN+MSVknPoh5hsqDY1ESL20qgOabLg2RbyKE/femWLHj8dvP2GwZbV2MC6KIeQRscNzXPtkTI8dYZH6I+ku1yyGazFjq8KShojdmcoxe+wzmbg6X9FUi/zGtVc0b+bZl1Ga0O8j1ATXt5WoUWAdB04t89/1O/w1cDnyilFU=')
 
 # Channel Secret
 
-handler = WebhookHandler('a13be1528f294201578d36297fc549a6')
+handler = WebhookHandler('b38e05865272e44156c19da5d5191c83')
 
 #===========[ NOTE SAVER ]=======================
 
@@ -106,29 +105,19 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-
 def handle_message(event):
-
     text = event.message.text #simplify for receove message
-
     sender = event.source.user_id #get usesenderr_id
-
     gid = event.source.sender_id #get group_id
-
     profile = line_bot_api.get_profile(sender)
-
     if text=="adit":
-
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat adit'))
-
+    if text=="rey":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Apa?'))
     if text=="mail":
-
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
-
     if text=="djohan":
-
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
-
 
 
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
