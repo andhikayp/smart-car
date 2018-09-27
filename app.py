0f397013ef235238ed9d972e1318a8a4d06b0c18
andhikayp <andhikay24@gmail.com>
2018-09-27 10:58:20 +0700

    a

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
     #line_bot_api.push_message(to, TextSendMessage(text='Hello World!'))
     #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.user_id+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.user_id+ +profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
 
 import os
 
