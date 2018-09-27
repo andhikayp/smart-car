3b24cb823ce3df323679d8f8b18b769f567e1bd7
andhikayp <andhikay24@gmail.com>
2018-09-27 10:08:18 +0700

    all

--- app.py
+++ app.py
@@ -1,108 +1,53 @@
 from flask import Flask, request, abort
-
 from linebot import (
-
     LineBotApi, WebhookHandler
-
 )
-
 from linebot.exceptions import (
-
     InvalidSignatureError
-
 )
-
 from linebot.models import *
-
 import requests, json
-
-
-
-
-
 import errno
-
 import os
-
 import sys, random
-
 import tempfile
-
 import requests
-
 import re
 
-
-
 from linebot.models import (
-
     MessageEvent, TextMessage, TextSendMessage,
-
     SourceUser, SourceGroup, SourceRoom,
-
     TemplateSendMessage, ConfirmTemplate, MessageAction,
-
     ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
-
     PostbackAction, DatetimePickerAction,
-
     CarouselTemplate, CarouselColumn, PostbackEvent,
-
     StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
-
     ImageMessage, VideoMessage, AudioMessage, FileMessage,
-
     UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
-
     FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
-
     TextComponent, SpacerComponent, IconComponent, ButtonComponent,
-
     SeparatorComponent,
-
 )
 
-
-
 app = Flask(__name__)
-
-
-
 # Channel Access Token
 line_bot_api = LineBotApi('t5b5X0LHwCZQHy55X75+FZfjBWBDfvw+NWgDIruLX/EoslSALtJHPdaBsVRlkjFUeQRscNzXPtkTI8dYZH6I+ku1yyGazFjq8KShojdmcowVeXZYWNjkd7oT5nAxHZIK8q+lM/KR0DM+IwdyO5lyGQdB04t89/1O/w1cDnyilFU=')
-
 # Channel Secret
-
 handler = WebhookHandler('b38e05865272e44156c19da5d5191c83')
 
 #===========[ NOTE SAVER ]=======================
-
 notes = {}
-
-
 # Post Request
-
 @app.route("/callback", methods=['POST'])
-
 def callback():
-
     signature = request.headers['X-Line-Signature']
-
     body = request.get_data(as_text=True)
-
     app.logger.info("Request body: " + body)
-
     try:
-
         handler.handle(body, signature)
-
     except InvalidSignatureError:
-
         abort(400)
-
     return 'OK'
-
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -125,15 +70,11 @@ def handle_message(event):
     if text=="djohan":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
 
-
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
-
-
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    #line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
 
 import os
 
 if __name__ == "__main__":
-
     port = int(os.environ.get('PORT', 5000))
-
     app.run(host='0.0.0.0', port=port)
