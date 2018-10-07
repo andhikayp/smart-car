f9a4bb0d4e41e5aaefbfe15458ae9ea97ea36b24
andhikayp <andhikay24@gmail.com>
2018-10-07 20:52:04 +0700

    wsq

--- app.py
+++ app.py
@@ -39,6 +39,98 @@ handler = WebhookHandler('b38e05865272e44156c19da5d5191c83')
 
 #===========[ NOTE SAVER ]=======================
 notes = {}
+
+#REQUEST DATA MHS
+def carimhs(nrp):
+    URLmhs = "http://www.aditmasih.tk/api-hafid/show.php?nrp=" + nrp
+    r = requests.get(URLmhs)
+    data = r.json()
+    err = "data tidak ditemukan"
+    
+    flag = data['flag']
+    if(flag == "1"):
+        nrp = data['data_angkatan'][0]['nrp']
+        nama = data['data_angkatan'][0]['nama']
+        kos = data['data_angkatan'][0]['kosan']
+
+        # munculin semua, ga rapi, ada 'u' nya
+        # all_data = data['data_angkatan'][0]
+        data= "Nama : "+nama+"\nNrp : "+nrp+"\nKosan : "+kos
+        return data
+        # return all_data
+
+    elif(flag == "0"):
+        return err
+
+#INPUT DATA MHS
+def inputmhs(nrp, nama, kosan):
+    r = requests.post("http://www.aditmasih.tk/api-hafid/insert.php", data={'nrp': nrp, 'nama': nama, 'kosan': kosan})
+    data = r.json()
+
+    flag = data['flag']
+   
+    if(flag == "1"):
+        return 'Data '+nama+' berhasil dimasukkan\n'
+    elif(flag == "0"):
+        return 'Data gagal dimasukkan\n'
+
+def allmhs():
+    r = requests.post("http://www.aditmasih.tk/api-hafid/all.php")
+    data = r.json()
+
+    flag = data['flag']
+   
+    if(flag == "1"):
+        hasil = ""
+        for i in range(0,len(data['data_angkatan'])):
+            nrp = data['data_angkatan'][int(i)][0]
+            nama = data['data_angkatan'][int(i)][2]
+            kos = data['data_angkatan'][int(i)][4]
+            hasil=hasil+str(i+1)
+            hasil=hasil+".\nNrp : "
+            hasil=hasil+nrp
+            hasil=hasil+"\nNama : "
+            hasil=hasil+nama
+            hasil=hasil+"\nKosan : "
+            hasil=hasil+kos
+            hasil=hasil+"\n"
+        return hasil
+    elif(flag == "0"):
+        return 'Data gagal dimasukkan\n'
+
+
+#DELETE DATA MHS
+def hapusmhs(nrp):
+    r = requests.post("http://www.aditmasih.tk/api-hafid/delete.php", data={'nrp': nrp})
+    data = r.json()
+
+    flag = data['flag']
+   
+    if(flag == "1"):
+        return 'Data '+nrp+' berhasil dihapus\n'
+    elif(flag == "0"):
+        return 'Data gagal dihapus\n'
+
+def updatemhs(nrpLama,nrp,nama,kosan):
+    URLmhs = "http://www.aditmasih.tk/api-hafid/show.php?nrp=" + nrpLama
+    r = requests.get(URLmhs)
+    data = r.json()
+    err = "data tidak ditemukan"
+    nrp_lama=nrpLama
+    flag = data['flag']
+    if(flag == "1"):
+        r = requests.post("http://www.aditmasih.tk/api-hafid/update.php", data={'nrp': nrp, 'nama': nama, 'kosan': kosan, 'nrp_lama':nrp_lama})
+        data = r.json()
+        flag = data['flag']
+
+        if(flag == "1"):
+            return 'Data '+nrp_lama+'berhasil diupdate\n'
+        elif(flag == "0"):
+            return 'Data gagal diupdate\n'
+
+    elif(flag == "0"):
+        return err
+
 # Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -79,7 +171,23 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://azurlane.koumakan.jp/w/images/d/d8/San_Diego.png',preview_image_url='https://azurlane.koumakan.jp/w/images/d/d8/San_Diego.png'))
     if text=="Google Center":
         line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='Mountain View, California', address='United State of America',latitude=37.4225195,longitude=-122.0847433))
-    if text=="bye":
+    
+    data=text.split('-') #pemisah
+    if(data[0]=='lihat'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=carimhs(data[1])))
+    elif(data[0]=='tambah'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=inputmhs(data[1],data[2],data[3])))
+    elif(data[0]=='hapus'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hapusmhs(data[1])))
+    elif(data[0]=='ganti'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=updatemhs(data[1],data[2],data[3],data[4])))
+    elif(data[0]=='semwa'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=allsmhs()))
+    elif(data[0]=='menu'):
+        menu = "1. lihat-[nrp]\n2. tambah-[nrp]-[nama]-[kosan]\n3. hapus-[nrp]\n4. ganti-[nrp lama]-[nrp baru]-[nama baru]-[kosan baru]\n5. semwa"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=menu))
+    
+    elif text=="bye":
         if isinstance(event.source, SourceGroup):
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Leaving group'))
             line_bot_api.leave_group(event.source.group_id)
