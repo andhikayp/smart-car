af91bd90da297777b5b40e4b110c87e0835e84da
andhikayp <andhikay24@gmail.com>
2018-10-08 11:34:53 +0700

    nbn

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def handle_message(event):
         inia="Hypertext"
         inib="telolet"
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=inia+inib))
-         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=inib))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=inib))
     if text=="rey":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://azurlane.koumakan.jp/w/images/d/d8/San_Diego.png',preview_image_url='https://azurlane.koumakan.jp/w/images/d/d8/San_Diego.png'))
     if text=="Google Center":
