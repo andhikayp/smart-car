7337212c5aa126e97e3c20c53fc6013bf978fa59
andhikayp <andhikay24@gmail.com>
2018-10-11 18:53:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -136,10 +136,7 @@ def handle_message(event):
     #    if isinstance(event.source, SourceRoom):
             #line_bot_api.push_message(event.source.room_id,TextSendMessage(text=data[x]))
             #x=x+1
-         #   else if isinstance(event.source, SourceGroup):
-          #      line_bot_api.push_message(event.source.group_id,TextSendMessage(text=data[x]))
-           # else:
-            #    line_bot_api.push_message(event.source.user_id,TextSendMessage(text=data[x]))
+         #   
            
     elif text=="/bye":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Pingin ngekick aku?:(\nketik "/start" gawe ngekick!'))
@@ -158,10 +155,14 @@ def handle_message(event):
      #   line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Hai,' +profile.display_name+'. Bahasa opo iki?\n'+event.message.text+'\nKok gak jelas banget'))
     #line_bot_api.multicast(['U8d343d76a1c15caad6dba2d2b5dab241'], TextSendMessage(text='Selamat Siang!'))
     
-    x=0
-    while  x < len(data):
+    x=1
+    while  x <= len(data):
         if isinstance(event.source, SourceRoom):
             line_bot_api.push_message(event.source.room_id,TextSendMessage(text=data[x]))
+        else if isinstance(event.source, SourceGroup):
+            line_bot_api.push_message(event.source.group_id,TextSendMessage(text=data[x]))
+        else:
+            line_bot_api.push_message(event.source.user_id,TextSendMessage(text=data[x]))
         x=x+1 
     #for x in data:
      #   line_bot_api.push_message(event.source.user_id, TextSendMessage(text=data[x]))
