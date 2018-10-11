1a381fc693a24ad5ff21da439d69656696f5cb2a
andhikayp <andhikay24@gmail.com>
2018-10-11 19:04:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -148,9 +148,9 @@ def handle_message(event):
     #elif not(isinstance(event.source, SourceGroup) or isinstance(event.source, SourceRoom)):
      #   line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Hai,' +profile.display_name+'. Bahasa opo iki?\n'+event.message.text+'\nKok gak jelas banget'))
     #line_bot_api.multicast(['U8d343d76a1c15caad6dba2d2b5dab241'], TextSendMessage(text='Selamat Siang!'))
-    elif (data[0]=='/spamkata'):
+    elif (data2[0]=='/spamkata'):
         x=1
-        while  x <= len(data):
+        while  x <= len(data2):
             if isinstance(event.source, SourceRoom):
                 line_bot_api.push_message(event.source.room_id,TextSendMessage(text=data2[x]))
             elif isinstance(event.source, SourceGroup):
