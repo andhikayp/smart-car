b19207824fc3b23acb199d8ac709936bf0b97aed
andhikayp <andhikay24@gmail.com>
2018-10-11 13:26:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -117,9 +117,9 @@ def handle_message(event):
     elif(data[0]=='semwa'):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=allsmhs()))
 
-    #x=0
-    #for x in data:
-        #line_bot_api.push_message(event.source.user_id, TextSendMessage(text=carimhs(data[x])))
+    x=0
+    for x in data:
+        line_bot_api.push_message(event.source.user_id, TextSendMessage(text=carimhs(data[x])))
 
     elif(data[0]=='/sangar'):
         pro = "Wong suroboyo terkenal karo kesangarane. Sak piro sangarmu cak?\n1. lihat-[id]\n2. tambah-[id]-[kesangaran]\n3. hapus-[id]\n4. ganti-[id lama]-[id baru]-[kesangaran baru]\n5. kabeh"
